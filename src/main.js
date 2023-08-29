dcc56354d9a22c77dcf9a782187d67549e3fe946
Arctrum <bradyhession@gmail.com>
2023-08-28 19:08:26 -0600

    Fixed dark mode

--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ createApp(App).use(router).use(BackToTop).mount('#app')
 const appTheme = localStorage.getItem('theme')
 
 // Check what is the active theme and change theme when user clicks on the theme button in header.
-if (appTheme === 'dark' && document.querySelector('body').classList.contains('app-theme')) {
-  document.querySelector('body').classList.add('bg-primary-dark')
+if (appTheme === 'dark') {
+  document.getElementById("mainBody").classList.add('bg-primary-dark')
 } else {
-  document.querySelector('body').classList.add('bg-secondary-light')
+  document.getElementById("mainBody").classList.add('bg-primary-light')
 }
